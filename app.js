b111df4a80882bc8729edfb4f76ee2f94598427c
mana0306 <33055349+mana0306@users.noreply.github.com>
2018-04-27 06:41:29 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
+
+var index = require('./routes/index');
+var login = require('./routes/login');
+var register = require('./routes/register');
+
+var managerindex = require('./routes/managerindex');
+
+var adminindex = require('./routes/adminindex');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,27 +24,39 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.use('/', index);
+app.use('/login', login);
+app.use('/register', register);
 
-app.all('/*', function (req, res) {
-    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
-});
+app.use('/managerindex', managerindex);
 
-// catch 404 and forward to error handler
+app.use('/adminindex', adminindex);
+
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+
   res.status(err.status || 500);
   res.render('error');
 });
 
+app.use(function (req, res) {
+    console.log(req.path);
+    if(req.path.indexOf('/api')>=0){
+        res.send("server text");
+
+    }else{ //angular启动页
+        res.sendfile('index.ejs');
+    }
+});
+
+app.listen(3001);
 module.exports = app;
