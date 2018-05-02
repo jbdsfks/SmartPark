4535223579877df706fa7e400b43aed6781a5f79
mana0306 <33055349+mana0306@users.noreply.github.com>
2018-05-02 09:49:55 +0800

    Update and rename managerindex.js to highwayindex2.js

--- routes/highwayindex2.js
+++ routes/highwayindex2.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('managerindex', { title: '管理员' });
+  res.render('highwayindex2', { title: '收费站管理' });
 });
 
 module.exports = router;
