1ca13417ec6964d36fbb0cb79c042fd2017c72d5
mana0306 <33055349+mana0306@users.noreply.github.com>
2018-05-02 09:48:52 +0800

    Update and rename index.js to parkindex.js

--- routes/parkindex.js
+++ routes/parkindex.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '首页' });
+  res.render('parkindex', { title: '停车场管理' });
 });
 
 module.exports = router;
