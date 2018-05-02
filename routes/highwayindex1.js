281ef6fbe06f4d6cc03f5d42f9b8522117fac02b
mana0306 <33055349+mana0306@users.noreply.github.com>
2018-05-02 09:49:28 +0800

    Update and rename adminindex.js to highwayindex1.js

--- routes/highwayindex1.js
+++ routes/highwayindex1.js
@@ -1,11 +1,9 @@
-
-
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('adminindex', { title: 'admin' });
+  res.render('highwayindex1', { title: '收费站管理' });
 });
 
 module.exports = router;
