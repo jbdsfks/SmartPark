6ea5e5681c6148bf0b58be2555d0a794fd8e8d5f
mana0306 <33055349+mana0306@users.noreply.github.com>
2018-05-02 14:32:09 +0800

    Update login-controller.js

--- public/controllers/login-controller.js
+++ public/controllers/login-controller.js
@@ -1,63 +1,55 @@
 /*
 *
-* loginPark(userid,password) ——停车场管理员登录，成功返回true
+* loginPark(username,password) ——停车场管理员登录，成功返回true
 * 
-* loginHighwayWorker(userid,password) ——收费站工作登人员登录，成功返回true
+* loginHighwayWorker(username,password) ——收费站工作登人员登录，成功返回true
 * 
-* loginHighwayAdmin(userid,password) ——收费站admin登录，成功返回true
+* loginHighwayAdmin(username,password) ——收费站admin登录，成功返回true
 */
 
-app.controller('loginController',function($scope,$location){
 
-    $scope.clickInfo=function(){
-        $scope.errorInfo='';
+app.controller ('loginController',function ($scope,$location) {
+
+    $scope.clickInfo = function () {
+        $scope.errorInfo = '';
     }
-    $scope.userid='';
-    $scope.password='';
-    $scope.usertype='';
+    $scope.username = '';
+    $scope.password = '';
+    $scope.usertype = '';
 
+    $scope.user=["停车场管理员", "收费站工作人员", "收费站admin"];
     $scope.login=function(){
-        if($scope.userid){
+        if($scope.username){
             if($scope.password){
-                if($scope.usertype){
-                    if($scope.usertype=='park-manager'){
-                        //var f=loginPark($scope,userid,$scope.password);
-                        if(true){
-                            localStorage.user=$scope.userid;
-                            localStorage.cookie="userid";
-                            $location.path('/parkindex');
-                        }else{
-                            alert('登录失败！';)
-                        }
-                    }else if ($scope.usertype=='highway-worker'){
-                        //var f=loginHighwayWorker($scope,userid,$scope.password);
-                        if(true){
-                            localStorage.user=$scope.userid;
-                            localStorage.cookie="userid";
-                            $location.path('/highwayindex1');
-                        }else{
-                            alert('登录失败！';)
-                        }
-                    }else{
-                        //var f=loginHighwayAdmin($scope,userid,$scope.password);
-                        if(true){
-                            localStorage.user=$scope.userid;
-                            localStorage.cookie="userid";
-                            $location.path('/highwayindex2');
-                        }else{
-                            alert('登录失败！';)
-                        }
+                if($scope.usertype==$scope.user[0]){
+                    //var f=loginPark(username,password)
+                    if(true){
+                        localStorage.user=$scope.username;
+                        localStorage.cookie="username";
+                        $location.path('/parkindex');
+                    }
+                }else if($scope.usertype==$scope.user[1]){
+                    //var f=loginHighwayWorker(username,password);
+                    if(true){
+                        localStorage.user=$scope.username;
+                        localStorage.cookie="username";
+                        $location.path('/highwayindex1');
                     }
                 }else{
-                    $scope.errorInfo='用户类型不能为空！';
+                    //var f=loginHighwayAdmin(username,password);
+                    if(true){
+                        localStorage.user=$scope.username;
+                        localStorage.cookie="username";
+                        $location.path('/highwayindex2');
+                    }
                 }
-                
             }else{
-                $scope.errorInfo='密码不能为空！';
+                $scope.errorInfo = '密码不能为空！';
             }
         }else{
-            $scope.errorInfo='用户名不能为空！';
+           $scope.errorInfo = '用户名不能为空！';
         }
     }
+
     
 });
