8df46b90d58129e09fcd32a29b165cc813a50b72
jbdsfks <jiang19950725>
2018-05-09 22:04:52 +0800

    check UI

--- app.js
+++ app.js
@@ -1,58 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-
-var login = require('./routes/login');
-var register = require('./routes/register');
-var parkindex = require('./routes/parkindex');
-var highwayindex1 = require('./routes/highwayindex1');
-var highwayindex2 = require('./routes/highwayindex2');
-
+var mongoose = require('./src/config/mongoose'),
+    passport = require('./src/config/passport'),
+    express = require('./src/config/express'),
+    path = require('path');
+var db = mongoose();
 var app = express();
 
-app.set('views', path.join(__dirname, 'views'));
+
+app.set('views',path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use('/', login);
-app.use('/register', register);
-app.use('/parkindex', parkindex);
-app.use('/highwayindex1', highwayindex1);
-app.use('/highwayindex2', highwayindex2);
-
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-
-app.use(function(err, req, res, next) {
-
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-app.use(function (req, res) {
-    console.log(req.path);
-    if(req.path.indexOf('/api')>=0){
-        res.send("server text");
-
-    }else{ //angular启动页
-        res.sendfile('login.ejs');
-    }
-});
-
-app.listen(3001);
+var passport = passport();
 module.exports = app;
--- src/config/express.js
+++ src/config/express.js
@@ -10,6 +10,12 @@ var config = require('./config'),
     flash = require('connect-flash'),
     passport = require('passport');
 
+var login = require('../../routes/login');
+var register = require('../../routes/register');
+var parkindex = require('../../routes/parkindex');
+var highwayindex1 = require('../../routes/highwayindex1');
+var highwayindex2 = require('../../routes/highwayindex2');
+
 module.exports = function() {
     var app = express();
 
@@ -18,7 +24,7 @@ module.exports = function() {
     } else if (process.env.NODE_ENV === 'production') {
         app.use(compression());
     }
-
+    app.use(express.static(path.join(__dirname,'../../public')));
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(cookieParser());
@@ -29,17 +35,21 @@ module.exports = function() {
     }));
     app.use(methodOverride());
 
+    // console.log();
 
-    app.set('views', './src/app/views');
-    app.set('view engine', 'ejs');
-    app.use(express.static(path.join(__dirname, 'public')));
 
     app.use(flash());
     app.use(passport.initialize());
     app.use(passport.session());
-    require('../app/routes/index.server.route')(app);
-    require('../app/routes/users.server.route')(app);
-    require('../app/routes/car.server.route')(app);
+
+    app.use('/', login);
+    app.use('/register', register);
+    app.use('/parkindex', parkindex);
+    app.use('/highwayindex1', highwayindex1);
+    app.use('/highwayindex2', highwayindex2);
+    // require('../app/routes/index.server.route')(app);
+    // require('../app/routes/users.server.route')(app);
+    // require('../app/routes/car.server.route')(app);
 
     app.use(express.static('./public'));
 // app.use('/', indexRouter);
